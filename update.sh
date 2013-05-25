54fda0ca31bd3e3283be1c4d262e1014dec0be1b
root <root@sdpi.no-ip.org>
2013-05-25 22:58:49 +0200

    Fixed update script

--- update.sh
+++ update.sh
@@ -3,25 +3,18 @@
 cd /var/www/piwake3/
 
 
-# VCS
 for i in {21..26}
 do
-wget -O VCS/vcs-$i --post-data 'PHPSESSID=v0dicsge4p47ugnrsvbfa911r5' "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/gpu2vcs.php?semaine=$i&prof_etu=FIL&filiere=SRC_S2"
+wget -O vcs/vcs-$i --post-data 'PHPSESSID=v0dicsge4p47ugnrsvbfa911r5' "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/gpu2vcs.php?semaine=$i&prof_etu=FIL&filiere=SRC_S2"
 done
 
-cd VCS
+clear > vcs/VCSALL
 
-echo '0' > VCSALL
+cat vcs/vcs* >> vcs/VCSALL
 
-cat vcs* >> VCSALL
-
-#echo 'Mise à jour VCS ok :' `date` >> ../log.txt
+echo 'Mise à jour VCS ok :' `date` >> log.txt
 
 
-#// EDT
-#for i in {21..26}
-#do
-#wget -O EDT/EDT-$i --post-data "PHPSESSID=v0dicsge4p47ugnrsvbfa911r5&semaine=$i" "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/index.php?page_param=fpfilieres.php&cat=0&numpage=1&niv=2&clef=/305/306/"
-#done
+script/updateDB.php
 
-#echo 'Mise à jour EDT ok :' `date` >> log.txt
+echo 'Mise à jour DB ok :' `date` >> log.txt
