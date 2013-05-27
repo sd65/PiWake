05d590272f23277920f152e27c949b3b74464a0e
Sylvain <sylvain.doignon@gmail.com>
2013-05-27 10:30:28 +0200

    Same code but better look

--- update.sh
+++ update.sh
@@ -2,21 +2,26 @@
 
 # This file is intented to be used in crontab
 
+# Fetch VCS files
 cd /var/www/piwake3/
-
 
 for i in {21..26}
 do
-wget -O vcs/vcs-$i --post-data 'PHPSESSID=v0dicsge4p47ugnrsvbfa911r5' "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/gpu2vcs.php?semaine=$i&prof_etu=FIL&filiere=SRC_S2"
+	wget -O vcs/vcs-$i --post-data 'PHPSESSID=v0dicsge4p47ugnrsvbfa911r5' "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/gpu2vcs.php?semaine=$i&prof_etu=FIL&filiere=SRC_S2"
 done
 
-clear > vcs/VCSALL
 
+# Update the files
+clear > vcs/VCSALL
 cat vcs/vcs* >> vcs/VCSALL
 
-echo 'Mise à jour VCS ok :' `date` >> log.txt
 
-
+# Fill the database
 script/updateDB.php
 
-echo 'Mise à jour DB ok :' `date` >> log.txt
+
+#Log
+echo 'Mise à jour ok :' `date` >> log.txt
+
+
+
