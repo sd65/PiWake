9ae3ec369891bf877c93759472090a084b2b9884
root <root@SDPC>
2013-05-28 16:38:43 +0200

    New input

--- index.php
+++ index.php
@@ -6,24 +6,28 @@ if (!empty($_POST)) // Si on soumet le formulaire
 	setcookie('enreg_semaine', $_POST['semaine'], time() + 365*24*3600, null, null,false, true); 
 	setcookie('enreg_tp', $_POST['tp'], time() + 365*24*3600, null, null,false, true); 
 	setcookie('enreg_td', $_POST['td'], time() + 365*24*3600, null, null,false, true); 
-
+	setcookie('enreg_vue', $_POST['vue'], time() + 365*24*3600, null, null,false, true); 
+	
 	$td = $_POST['td'] ;
 	$tp = $_POST['tp'] ;
 	$semaine = $_POST['semaine'] ;
+	$vue = $_POST['vue'] ;
 }
 else
 {
 	if (!empty($_COOKIE)) //Si on revient sur PiWake avec des Cookies remplis
 	{
-	$td = $_COOKIE['enreg_td'] ;
+		$td = $_COOKIE['enreg_td'] ;
         $tp = $_COOKIE['enreg_tp'] ;
         $semaine = $_COOKIE['enreg_semaine'] ;
+        $vue = $_COOKIE['enreg_vue'] ;
 	}
 	else
 	{
-	$td = 1 ;
+		$td = 1 ;
         $tp = 1 ;
         $semaine = date('W') ;
+        $vue = 1 ;
 	}
 
 }
@@ -35,6 +39,7 @@ $bdd = new PDO("sqlite:../PIWAKE");
 
 //Link
 include_once "functions.php" ;
+
 
 ?>
 
@@ -65,6 +70,14 @@ include_once "functions.php" ;
 		<div id="formulaire"> 
 			
 			<form action="index.php" method="post">
+			
+			<label for="vue">Vue :</label>
+			<select id="vue" name="vue" onchange="this.form.submit()">
+				<option <?php if($vue==1){echo "selected" ;}?> value="0">Sélective</option>
+				<option <?php if($vue==2){echo "selected" ;}?> value="1">Générale</option>
+				<option value="1">TEST</option>
+				<option  value="1">EN COURS</option>
+			</select>
 		
 			<label for="tp">TP :</label>
 			<select id="selectTP" name="tp" onchange="this.form.submit()">
