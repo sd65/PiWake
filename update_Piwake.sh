b4e313768a58953f8009e4a95e54f1f099551f2f
Sylvain <s911141111@gmail.com>
2013-09-21 00:51:31 +0200

    Cleaned code for better performance and simplicity

--- update_Piwake.sh
+++ update_Piwake.sh
@@ -2,30 +2,28 @@
 
 # This file is intented to be used in crontab
 
-# Fetch VCS files
+# Fetch folder
 cd /usr/share/nginx/www/piwake/
 
-# Login to Sattelys Server
-
+# Getting Cookie on Sattelys Server
 wget -O /dev/null --keep-session-cookies --save-cookies "cookie/cookie" --post-data "modeconnect=connect&util=sdoignon&acct_pass=123" 'http://syrah.iut.u-bordeaux3.fr/gpu/sat/index.php?page_param=accueilsatellys.php&cat=0&numpage=1&niv=0&clef=/'
 
-for i in {38..52}
+# Using it
+for (( week=$(date +"%V") ; week<=52 ; week++))
 do
-	wget -O vcs/vcs-$i --load-cookies cookie/cookie "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/index.php" "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/gpu2vcs.php?semaine=$i&prof_etu=FIL&filiere=SRC_S3"
+	wget -O vcs/vcs-$week --load-cookies "cookie/cookie" "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/index.php" "http://syrah.iut.u-bordeaux3.fr/gpu/gpu/gpu2vcs.php?semaine=$week&prof_etu=FIL&filiere=SRC_S3"
 done
-
 
 # Update the files
 clear > vcs/VCSALL
-cat vcs/vcs* >> vcs/VCSALL
+cat vcs/vcs-* >> vcs/VCSALL
 
 
 # Fill the database
 script/updateDB.php
 
-
 #Log
-echo 'Mise à jour ok :' `date` >> log.txt
+echo 'Mise à jour :' `date` >> log/log.txt
 
 
 
